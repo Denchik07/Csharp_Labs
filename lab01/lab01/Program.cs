9a9549f8c1260f0b2edb6223972486106fd2ce97
Denchik07 <87420695+Denchik07@users.noreply.github.com>
2023-01-10 16:14:32 +0200

    Add files via upload

---
+++ lab01/lab01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double height, quick_fall;
+        quick_fall = 9.81;
+        
+        Console.Write("Введіть висоту в метрах з якої падає тіло:");
+        
+        height = double.Parse(Console.ReadLine());
+        double timefall = Math.Sqrt(2 * height / quick_fall);
+        
+        Console.WriteLine($"Тіло впаде через {timefall} секунди");
+        
+        Console.ReadLine();
+    }
+}
