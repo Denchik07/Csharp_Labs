e5be0791a79ab500440c8d743a2e11bd076566ac
Denchik07 <87420695+Denchik07@users.noreply.github.com>
2023-01-11 20:40:38 +0200

    Add files via upload

---
+++ lab02/lab02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab_06
+{
+    class Program
+    {
+        static double Sum(double nn, double nk)
+        {
+            double sum = 0;
+
+            while (nn <= nk)
+            {
+                double a = (Math.Pow(nn,2)+Math.Pow((-1),nn-1)*(2*nn)-1)/
+                           (Math.Pow(nn,2)-8);
+                sum += a;
+                nn++;
+            }
+
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть nn та nk");
+            Console.WriteLine(Sum(Convert.ToDouble(Console.ReadLine()),Convert.ToDouble(Console.ReadLine())));
+        }
+    }
+}
